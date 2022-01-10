6715479f92be9003a7ef9edc24a1d67df19c4469
Nguyen Thi Thu Thao <thao.chip.0305@gmail.com>
2022-01-10 23:14:04 +0700

    run server

---
+++ run.sh
@@ -0,0 +1,1 @@
+pm2 start "node ./server.js" --name server
--- server.js
+++ server.js
@@ -17,6 +17,6 @@
 var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/public')); //__dir and not _dir
-var port = 443; // you can use any port
+var port = 8080; // you can use any port
 app.listen(port);
 console.log('server on' + port);
